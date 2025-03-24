64dc70188c97e3cea63c75205625ee068d380e2b
HaojieZhang123 <haojiezhang123@gmail.com>
2025-03-24 16:09:34 +0100

    [UPD] updated contavocali with an output more fitting with expected results

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -6,17 +6,19 @@ const word = 'javascript';
 // Dichiara la funzione qui.
 function contaVocali(str){
     let count = 0;
+    const strVowels = []
     for(let i = 0; i < str.length; i++){
         if(str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u'){
             count++;
+            strVowels.push(str[i]);
         }
     }
-    return count;
+    return strVowels;
 }
 
 
 // Invoca la funzione qui e stampa il risultato in console
-console.log(contaVocali(word));
+console.log(contaVocali(word).length + ' (' + contaVocali(word) + ')');
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
