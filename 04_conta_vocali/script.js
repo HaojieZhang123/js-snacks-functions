2a87aa9f599f7fac81bdde00af07c62f644f5226
HaojieZhang123 <haojiezhang123@gmail.com>
2025-03-24 15:44:14 +0100

    [UPD] 04 conta vocali done

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,10 +4,19 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function contaVocali(str){
+    let count = 0;
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === 'a' || str[i] === 'e' || str[i] === 'i' || str[i] === 'o' || str[i] === 'u'){
+            count++;
+        }
+    }
+    return count;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(contaVocali(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
